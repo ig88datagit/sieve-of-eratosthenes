d9bd10da2d3ac426464368e95842399c06fcf30c
Composer Games Dev <composer.games+github@gmail.com>
2016-11-06 11:06:38 -0500

    Break point

--- sieve.go
+++ sieve.go
@@ -3,32 +3,48 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math"
 )
 
-func ConsecutiveIntegers(n int) ([]bool, error) {
+func ConsecutiveIntegers(n int) ([][]bool, error) {
 	if n < 2 {
 		return nil, errors.New(fmt.Sprintf("%d is less than 2.", n))
 	}
 
-	return make([]bool, n), nil
+	if n < 64 {
+		r := make([][]bool, 1)
+		r[0] = make([]bool, n-2)
+		return r, nil
+	}
+
+	segsize := int(math.Sqrt(float64(n - 2)))
+	segcount := int(math.Ceil(float64(n-2) / float64(segsize)))
+
+	r := make([][]bool, segcount)
+	for i := 0; i < segcount; i++ {
+		r[i] = make([]bool, segsize)
+	}
+
+	return r, nil
 }
 
-func SieveOfEratosthenes(n int, nlist []bool) ([]int, error) {
-	// ensure count(numlist) - 2 >= n
-
+func SegmentedSieveOfEratosthenes(n int, nlist [][]bool) ([]int, error) {
 	var primes []int
 	var p int
+
 	for i := 0; i < n-2; i++ {
-		if nlist[i] == false {
-			// index is number - 2
-			p = i + 2
 
-			// we got a prime
-			primes = append(primes, p)
+		for j := 0; j < len(nlist); j++ {
+			for k := 0; k < len(nlist[j]); k++ {
+				if nlist[j][k] == false {
+					p = i + 2
 
-			// mark multiples
-			for j := 2; (j * p) < n; j++ {
-				nlist[j*p-2] = true
+					primes = append(primes, p)
+
+					for l := 2; (l * p) < len(nlist)*(j-1)*len(nlist[j-1])+len(nlist[j]); l++ {
+						nlist[j][l*p-2] = true
+					}
+				}
 			}
 		}
 	}
@@ -38,10 +54,10 @@ func SieveOfEratosthenes(n int, nlist []bool) ([]int, error) {
 
 func main() {
 	var con, _ = ConsecutiveIntegers(10)
+	var nlist, _ = SegmentedSieveOfEratosthenes(10, con)
 
-	var numlist, _ = SieveOfEratosthenes(10, con)
-
-	for i := 0; i < len(numlist); i++ {
-		fmt.Println(numlist[i])
+	for i := 0; i < len(nlist); i++ {
+		fmt.Println(nlist[i])
 	}
+	fmt.Printf("%v", con)
 }
--- sieve_test.go
+++ sieve_test.go
@@ -1,8 +1,19 @@
-package main
+package main_test
 
 import (
+	"fmt"
 	"testing"
 )
+
+func TestConsecutiveIntegers(t *testing.T) {
+	testCases := []int{1, 32, 64, 128, 256, 512, 2024, 4028, 8056}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%d", tc), func(T *testing.T) {
+
+		})
+	}
+}
 
 func BenchmarkSieveOfEratosthenes(b *testing.B) {
 	l, _ := ConsecutiveIntegers(b.N)
